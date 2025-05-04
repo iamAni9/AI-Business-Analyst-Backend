d8ee8bcda301a72ea6e9ea64637298e10e78d18b
harshdev-7275 <harsh.dev7275@gmail.com>
2025-05-04 12:11:05 +0530

    updated gitignore

--- dist/routes/dataRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dataController_1 = require("../controller/dataController");
-const multer_1 = require("../config/multer");
-const router = (0, express_1.Router)();
-router.post("/upload", multer_1.upload.single('file'), dataController_1.uploadData);
-router.get("/files", dataController_1.getAllUploadedFiles);
-exports.default = router;
--- dist/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userController_1 = require("../controller/userController");
-const router = (0, express_1.Router)();
-router.post("/signup", userController_1.signUpUser);
-router.post("/signIn", userController_1.signInUser);
-exports.default = router;
